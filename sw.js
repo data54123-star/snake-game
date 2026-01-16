70a327546bd76b0d44283729e4b0a31e7135150b
data54123-star <data54123@gmail.com>
2026-01-16 13:00:18 +0000

    Start

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'snake-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+// Installation: Cache aufbauen
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Aktivierung: Alte Caches löschen (falls Update)
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+// Fetch: Anfragen abfangen und aus Cache bedienen (Offline Support)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
